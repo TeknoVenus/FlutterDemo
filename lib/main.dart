ab26cd8d601f2e7c95882682378ff7b965f2faf1
Stephen Foulds <stephen.foulds@consult.red>
2024-01-15 16:02:43 +0000

    Keyboard monitoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,99 @@
 // ignore_for_file: unused_local_variable
 // #docregion ShakeCurve
-import 'dart:math';
 
 // #enddocregion ShakeCurve
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(const LogoApp());
+void main() => runApp(const HelloWorldApp());
 
-// #docregion diff
-class AnimatedLogo extends AnimatedWidget {
-  const AnimatedLogo({super.key, required Animation<double> animation})
-      : super(listenable: animation);
+class KeyboardMonitor extends StatefulWidget {
+  const KeyboardMonitor({
+    super.key,
+    required this.onKeyPress,
+    required this.child,
+  });
 
-  // Make the Tweens static because they don't change.
-  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
-  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+  final Widget child;
+  final Function(String?) onKeyPress;
+
+  @override
+  State<KeyboardMonitor> createState() => _KeyboardMonitorState();
+}
+
+class _KeyboardMonitorState extends State<KeyboardMonitor> {
+  @override
+  void initState() {
+    super.initState();
+    RawKeyboard.instance.addListener(_keyboardCallback);
+  }
+
+  @override
+  void dispose() {
+    RawKeyboard.instance.removeListener(_keyboardCallback);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final animation = listenable as Animation<double>;
-    return Center(
-      child: Opacity(
-        opacity: _opacityTween.evaluate(animation),
-        child: Container(
-          margin: const EdgeInsets.symmetric(vertical: 10),
-          height: _sizeTween.evaluate(animation),
-          width: _sizeTween.evaluate(animation),
-          child: const FlutterLogo(),
-        ),
-      ),
+    return widget.child;
+  }
+
+  void _keyboardCallback(RawKeyEvent keyEvent) {
+    if (keyEvent is! RawKeyDownEvent) return;
+
+    widget.onKeyPress(keyEvent.logicalKey.toString());
+  }
+}
+
+class KeyPressComponent extends StatefulWidget {
+  const KeyPressComponent({super.key});
+
+  @override
+  State<KeyPressComponent> createState() => _KeyPressComponentState();
+}
+
+class _KeyPressComponentState extends State<KeyPressComponent> {
+  // change to false, if you navigated to another page or opened a dialog.
+  String? _lastKeyPress = "Waiting for key press. . . ";
+
+  @override
+  Widget build(BuildContext context) {
+    return KeyboardMonitor(
+      onKeyPress: (String? key) {
+        setState(() {
+          _lastKeyPress = key;
+        });
+      },
+      child: Column(children: [
+        const SizedBox(height: 50),
+        const Image(image: AssetImage('assets/sky.png'), height: 150),
+        const SizedBox(height: 50),
+        const Text("Keyboard Monitor",
+            style: TextStyle(fontWeight: FontWeight.bold)),
+        const SizedBox(height: 30),
+        Text(_lastKeyPress!)
+      ]),
     );
   }
 }
 
-class LogoApp extends StatefulWidget {
-  const LogoApp({super.key});
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({Key? key}) : super(key: key);
 
   @override
-  State<LogoApp> createState() => _LogoAppState();
-}
-
-class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-  late Animation<double> animation;
-  late AnimationController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    // #docregion AnimationController, tweens
-    controller =
-        AnimationController(duration: const Duration(seconds: 3), vsync: this);
-    // #enddocregion AnimationController, tweens
-    animation = CurvedAnimation(parent: controller, curve: Curves.ease)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
-      });
-    controller.forward();
-  }
-
-  @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              title: const Text('EntOS Flutter Test App'),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blueGrey,
+            ),
+            body: DefaultTextStyle.merge(
+                style: const TextStyle(fontSize: 32),
+                child: const Center(child: KeyPressComponent())),
+            backgroundColor: Colors.white));
   }
 }
-// #enddocregion diff
-
-// Extra code used only in the tutorial explanations. It is not used by the app.
-
-class UsedInTutorialTextOnly extends _LogoAppState {
-  UsedInTutorialTextOnly() {
-    // ignore: prefer_typing_uninitialized_variables
-    var animation, sizeAnimation, opacityAnimation, tween, colorTween;
-
-    // #docregion CurvedAnimation
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn);
-    // #enddocregion CurvedAnimation
-
-    // #docregion tweens
-    sizeAnimation = Tween<double>(begin: 0, end: 300).animate(controller);
-    opacityAnimation = Tween<double>(begin: 0.1, end: 1).animate(controller);
-    // #enddocregion tweens
-
-    // #docregion tween
-    tween = Tween<double>(begin: -200, end: 0);
-    // #enddocregion tween
-
-    // #docregion colorTween
-    colorTween = ColorTween(begin: Colors.transparent, end: Colors.black54);
-    // #enddocregion colorTween
-  }
-
-  void usedInTutorialOnly1() {
-    // #docregion IntTween
-    AnimationController controller = AnimationController(
-        duration: const Duration(milliseconds: 500), vsync: this);
-    Animation<int> alpha = IntTween(begin: 0, end: 255).animate(controller);
-    // #enddocregion IntTween
-  }
-
-  void usedInTutorialOnly2() {
-    // #docregion IntTween-curve
-    AnimationController controller = AnimationController(
-        duration: const Duration(milliseconds: 500), vsync: this);
-    final Animation<double> curve =
-        CurvedAnimation(parent: controller, curve: Curves.easeOut);
-    Animation<int> alpha = IntTween(begin: 0, end: 255).animate(curve);
-    // #enddocregion IntTween-curve
-  }
-}
-
-// #docregion ShakeCurve
-class ShakeCurve extends Curve {
-  @override
-  double transform(double t) => sin(t * pi * 2);
-}
-// #enddocregion ShakeCurve
